6777271a9de5e875d35ae8334e48bd6f1f871364
adam.sernheim <adam@sernheim.com>
2019-12-01 09:40:54 +0100

    Restructure day 01

--- 01/main.nim
+++ 01/main.nim
@@ -1,16 +1,23 @@
 import strutils, os, math, sequtils
 
+proc computeFuel(mass: int): int = 
+  result = int(floor(float(mass) / 3)) - 2
+
 proc requiredFuel(m: int): int =
-  var fuel = m
-  while fuel >= 0:
-    #echo fuel, ' ', result
-    fuel = int(floor(float(fuel) / 3)) - 2
-    if fuel > 0:
-      result.inc(fuel)
+  result = computeFuel(m)
+  var fuel = result
+  while (fuel = computeFuel(fuel); fuel) > 0: 
+    result.inc(fuel)
 
+var input = readFile(paramStr(1))
 
+echo "Part 1: ", input
+  .splitLines()
+  .map(parseInt)
+  .map(computeFuel)
+  .foldl(a + b)
 
-echo readFile(paramStr(1))
+echo "Part 2: ", input
   .splitLines()
   .map(parseInt)
   .map(requiredFuel)
